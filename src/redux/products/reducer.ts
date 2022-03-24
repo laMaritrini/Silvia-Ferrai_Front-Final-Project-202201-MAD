3318f7e44dd37a52f53455d7b835459d2699713a
Silvia Ferrai <silviafer@hotmail.it>
2022-03-24 10:49:57 +0100

    Update eslint config and fixed reducer products

--- src/redux/products/reducer.ts
+++ src/redux/products/reducer.ts
@@ -5,7 +5,6 @@ import { actionTypesProduct } from './actionTypes';
 export function productsReducer(state: ArticleI[] = [], action: AnyAction) {
     if (action.type === actionTypesProduct.loadProducts) {
         return [...action.payload];
-    } else {
-        return state;
     }
+    return state;
 }
